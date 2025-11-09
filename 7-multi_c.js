253ebabf2cf35ebb990585b0b5c3f28f0f367337
Oweis-Nd <oweilayefandoni@gmail.com>
2025-11-09 05:51:24 +0100

    This script prints times C is fun

---
+++ 7-multi_c.js
@@ -0,0 +1,12 @@
+// A script that prints x times "C is fun"
+
+const args = process.argv.slice(2);
+const x = Number(args[0]);
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}

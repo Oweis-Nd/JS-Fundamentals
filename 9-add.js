ae6d272cdf19c9ad96443aab8d8e64e3fd16110e
Oweis-Nd <oweilayefandoni@gmail.com>
2025-11-09 05:54:39 +0100

    This script prints the addition of 2 integers

---
+++ 9-add.js
@@ -0,0 +1,11 @@
+// A script that prints the addition of 2 integers
+
+function add(a, b) {
+  return a + b;
+}
+
+const args = process.argv.slice(2);
+const num1 = Number(args[0]);
+const num2 = Number(args[1]);
+
+console.log(add(num1, num2));

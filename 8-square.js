452ea73e29c8741038336086227397ede27a1eb5
Oweis-Nd <oweilayefandoni@gmail.com>
2025-11-09 05:53:22 +0100

    This script prints a square

---
+++ 8-square.js
@@ -0,0 +1,12 @@
+// A script that prints a square using 'X'
+
+const args = process.argv.slice(2);
+const size = Number(args[0]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
